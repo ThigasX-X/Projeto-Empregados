30f8591fc443cd1c02a5bdbed4656baa90293552
Thiago Simões Felinto <thiagosfelinto@gmail.com>
2025-06-10 22:05:28 -0300

    Projeto-Empregado

--- Back/src/config/database.js
+++ Back/src/config/database.js
@@ -1,10 +1,10 @@
-import mysql from 'mysql2/promise'
+import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD, 
-    database: process.env.DB_NAME,
+    host: 'db',                      
+    user: 'root',                   
+    password: 'Tigas@133556',              
+    database: 'projeto_node',       
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
